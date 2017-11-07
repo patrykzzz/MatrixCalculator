25a00e08f55fc3457f1ff2cfdee4eaa90a2fc9ab
patryk <patrykmilek96@gmail.com>
2017-11-07 13:50:09 +0100

    Add working Matrix addition with test

---
+++ MatrixCalculator/Matrix.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MatrixCalculator
+{
+    public class Matrix<T> where T : struct
+    {
+        public static int NumberOfRows { get; set; }
+        public static int NumberOfColumns { get; set; }
+
+        public T[ , ] MatrixValues { get; set; }
+
+        public Matrix(T[ ,] matrix)
+        {
+            MatrixValues = matrix;
+            NumberOfRows = matrix.GetLength(0);
+            NumberOfColumns = matrix.GetLength(1);
+        }
+
+        public Matrix(int numberOfRows, int numberOfColumns)
+        {
+            NumberOfRows = numberOfRows;
+            NumberOfColumns = numberOfColumns;
+        }
+
+        private T[,] InitializeWithRandomNumbers()
+        {
+            var random = new Random();
+            var matrix = new T[NumberOfRows, NumberOfColumns];
+            for (int i = 0; i < NumberOfRows; i++)
+            {
+                for (int j = 0; j < NumberOfColumns; j++)
+                {
+                    matrix[i, j] = (dynamic)random.Next(Int32.MinValue, Int32.MaxValue);
+                }
+            }
+            return matrix;
+        }
+
+        public static Matrix<T> operator +(Matrix<T> i, Matrix<T> j)
+        {
+            return new Matrix<T>(Add(i, j));
+        }
+
+        public static T[ ,] Add(Matrix<T> a, Matrix<T> b)
+        {
+            var matrix = new T[NumberOfRows, NumberOfColumns];
+            for (int i = 0; i < NumberOfRows; i++)
+            {
+                for (int j = 0; j < NumberOfColumns; j++)
+                {
+                    matrix[i, j] = (dynamic)a.MatrixValues[i, j] + (dynamic)b.MatrixValues[i, j];
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ MatrixCalculator/MatrixTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace MatrixCalculator
+{
+    public class MatrixTests
+    {
+        [Fact]
+        public void Add_IntValues_ShouldReturnProperMatrix()
+        {
+            //Arrange
+            var x = new [,]
+            {
+                {1, 1, 1},
+                {2, 2, 2},
+                {3, 3, 3}
+            };
+            var a = new Matrix<int>(x);
+            var y = new [,]
+            {
+                {1, 1, 1},
+                {2, 2, 2},
+                {3, 3, 3}
+            };
+            var b = new Matrix<int>(y);
+            //Act
+            var result = a + b;
+
+            //Assert
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Debug.WriteLine(result.MatrixValues[i, j]);
+                }
+            }
+            Assert.Equal(result.MatrixValues, new [,]
+            {
+                { 2, 2, 2},
+                { 4, 4, 4},
+                { 6, 6, 6}
+            });
+        }
+
+    }
+}
