10eed6fd737353e19fa08fcd5729abe4d88de98a
Michal Niskowski <niski315@gmail.com>
2017-11-16 18:21:35 +0100

    add operations on matrix and writing to file

---
+++ MatrixCalculator/File.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixCalculator
+{
+    public class File<T>
+    {
+        public void WriteToFile(Matrix<T> matrix, int size, StreamWriter sw)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    sw.Write(matrix.MatrixValues[i, j] + " ");
+                }
+            }
+            sw.WriteLine();
+        }
+
+        public void WriteToFile(T[] vector, int size, StreamWriter sw)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                sw.Write(vector[i] + " ");
+            }
+            sw.WriteLine();
+        }
+
+        public T[] FillVectorWithRandom(int size)
+        {
+            var vector = new T[size];
+            var random = new Random();
+
+            if (vector is double[] || vector is float[])
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    vector[i] = (dynamic)random.NextDouble() * random.Next(Int32.MinValue, Int32.MaxValue);
+                }
+            }
+            else if (vector is Fraction[])
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    vector[i] = (dynamic)new Fraction(random.Next(Int32.MinValue, Int32.MaxValue), random.Next(Int32.MinValue, Int32.MaxValue));
+                }
+            }
+
+            return vector;
+        }
+    }
+}
--- MatrixCalculator/Program.cs
+++ MatrixCalculator/Program.cs
@@ -1,9 +1,74 @@
-﻿namespace MatrixCalculator
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace MatrixCalculator
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private readonly StreamWriter _firstMatrixFile = new StreamWriter("FirstMatrixData.txt");
+        private readonly StreamWriter _secondMatrixFile = new StreamWriter("SecondMatrixData.txt");
+        private readonly StreamWriter _thirdMatrixFile = new StreamWriter("ThirdMatrixData.txt");
+        private readonly StreamWriter _vectorFile = new StreamWriter("VectorData.txt");
+        private readonly StreamWriter _firstOp = new StreamWriter("FirstOperationData.txt");
+        private readonly StreamWriter _secOp = new StreamWriter("SecondOperationData.txt");
+        private readonly StreamWriter _thrdOp = new StreamWriter("ThirdOperationData.txt");
+
+
+        public static void Main(string[] args)
         {
+            var program = new Program();
+
+            program.CreateFillMatrixAndWriteToFileDouble(5);
+            program.CreateFillMatrixAndWriteToFileDouble(10);
+            program.CreateFillMatrixAndWriteToFileDouble(15);
+
+
+            program._firstMatrixFile.Close();
+            program._secondMatrixFile.Close();
+            program._vectorFile.Close();
+            program._firstOp.Close();
+            program._secOp.Close();
+            program._thrdOp.Close();
+
+            Console.ReadKey();
+
+        }
+
+        private void CreateFillMatrixAndWriteToFileDouble(int size)
+        {
+            var file = new File<double>();
+
+            var matrixA = new Matrix<double>(size, size);
+            var matrixB = new Matrix<double>(size, size);
+            var matrixC = new Matrix<double>(size, size);
+            double[] vector = file.FillVectorWithRandom(size);
+
+            file.WriteToFile(matrixA, size, _firstMatrixFile);
+            file.WriteToFile(matrixB, size, _secondMatrixFile);
+            file.WriteToFile(matrixC, size, _thirdMatrixFile);
+            file.WriteToFile(vector, size,  _vectorFile);
+
+            var res1 = matrixA * vector;
+            var res2 = (matrixA + matrixB + matrixC) * vector;
+            var res3 = matrixA * (matrixB * matrixC);
+
+            Console.Write(res1);
+
+            file.WriteToFile(res1, size, _firstOp);
+            file.WriteToFile(res2, size, _secOp);
+            file.WriteToFile(res3, size, _thrdOp);
+
+        }
+
+        private void CreateFillMatrixAndWriteToFileFloat(int size)
+        {
+
+        }
+
+        private void CreateFillMatrixAndWriteToFileFraction(int size)
+        {
+
         }
     }
 }
